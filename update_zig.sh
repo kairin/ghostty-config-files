4f6bb206933f0304ae221daa2f3d0c6028df22c4
Mr K - Home PC <678459+kairin@users.noreply.github.com>
2025-08-10 11:32:47 +0800

    fix: correct zig download url in update script

--- update_zig.sh
+++ update_zig.sh
@@ -3,25 +3,29 @@
 # This script automates the process of updating Zig to version 0.14.1.
 
 LATEST_VERSION="0.14.1"
+FILE_NAME="zig-linux-x86_64-$LATEST_VERSION"
+ARCHIVE_NAME="$FILE_NAME.tar.xz"
+DOWNLOAD_URL="https://ziglang.org/download/$LATEST_VERSION/$ARCHIVE_NAME"
+
 
 echo "Updating Zig to version $LATEST_VERSION"
 
 # Download the latest version
-wget "https://ziglang.org/download/$LATEST_VERSION/zig-linux-x86_64-$LATEST_VERSION.tar.xz"
+wget "$DOWNLOAD_URL"
 
 # Extract the archive
-tar -xf "zig-linux-x86_64-$LATEST_VERSION.tar.xz"
+tar -xf "$ARCHIVE_NAME"
 
 # Remove the old Zig installation
 sudo rm -rf /usr/local/zig
 
 # Move the new version
-sudo mv "zig-linux-x86_64-$LATEST_VERSION" /usr/local/zig
+sudo mv "$FILE_NAME" /usr/local/zig
 
 # Verify the new version
 zig version
 
 # Clean up the downloaded archive
-rm "zig-linux-x86_64-$LATEST_VERSION.tar.xz"
+rm "$ARCHIVE_NAME"
 
 echo "Zig has been updated to version $LATEST_VERSION"
